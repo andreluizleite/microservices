b77fd9738564d63cc528950532b61120736f9b52
andreluizleite <andreluiz_leite@hotmail.com>
2025-06-15 23:07:44 +0100

    Fixed: Expression = "Convert.ToInt32(Age) >= 18"

--- RuleEngine/src/RuleEngine.Application/Interfaces/IRuleCompilerService.cs
+++ RuleEngine/src/RuleEngine.Application/Interfaces/IRuleCompilerService.cs
@@ -1,9 +1,11 @@
-﻿using RuleEngine.Domain.Core.Rules;
+﻿using RuleEngine.Domain.Core.Interfaces;
+using RuleEngine.Domain.Core.Rules;
 
 namespace RuleEngine.Application.Interfaces
 {
     public interface IRuleCompilerService<T>
     {
         void Compile(IEnumerable<Rule<T>> rules);
+        void Compile(IRuleNode<T> node);
     }
 }
